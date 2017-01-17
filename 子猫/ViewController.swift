f67bf03d6076b51c692b7ff29aaa5fd21cdbda79
川口剛司 <zhei5acwyrtmhbxgzeu@gmail.com>
2017-01-17 20:10:03 +0900

    first commit

--- 子猫/ViewController.swift
+++ 子猫/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var label: UILabel!
+    @IBAction func goBack(_ segue:UIStoryboardSegue) {}
+    
+    @IBAction func goNext(_ sender:UIButton) {
+        let next = storyboard!.instantiateViewController(withIdentifier: "nextView")
+        self.present(next,animated: true, completion: nil)
+        
+    }
+    @IBAction func goNextBySegue(_ sender:UIButton) {
+        performSegue(withIdentifier: "nextSegue", sender: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        label.text = getNowClockString()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    func getNowClockString() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd' 'HH:mm:ss"
+        let now = Date()
+        return formatter.string(from: now)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
--- 子猫Tests/__Tests.swift
+++ 子猫Tests/__Tests.swift
@@ -7,7 +7,7 @@
 //
 
 import XCTest
-@testable import __
+@testable import
 
 class __Tests: XCTestCase {
     
@@ -28,7 +28,7 @@ class __Tests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
