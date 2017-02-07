8d691ac177f518249f3b2ac871d535820a0d2e29
川口剛司 <zhei5acwyrtmhbxgzeu@gmail.com>
2017-02-07 20:32:11 +0900

    近接sensor作った。ストーリーボードも組み替えた

--- 子猫/NextViewController.swift
+++ 子猫/NextViewController.swift
@@ -10,53 +10,33 @@ import UIKit
 
 
 class NextViewController: UIViewController {
-    
+        
     private var myTextField: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.white
-        let myDatePicker: UIDatePicker = UIDatePicker()
-        myDatePicker.frame = CGRect(x:0, y:50, width:self.view.frame.width, height:200)
-        myDatePicker.timeZone = NSTimeZone.local
-        myDatePicker.backgroundColor = UIColor.white
-        myDatePicker.layer.cornerRadius = 5.0
-        myDatePicker.layer.shadowOpacity = 0.5
-
-        myDatePicker.addTarget(self, action: #selector(NextViewController.onDidChangeDate(sender:)), for: .valueChanged)
-        
     }
-    internal func onDidChangeDate(sender: UIDatePicker){
-        let myDateFormatter: DateFormatter = DateFormatter()
-        myDateFormatter.dateFormat = "yyyy/MM/dd hh:mm"
-        
-        let mySelectedDate: NSString = myDateFormatter.string(from: sender.date) as NSString
-        myTextField.text = mySelectedDate as String
-    
-        
-        
-    }
-    
-    
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
-    }
-    
-    
+}
 
-    }
-    
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+
+
+
+
+
+
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destinationViewController.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+}
---
+++ 子猫/SensorViewController.swift
@@ -0,0 +1,70 @@
+//
+//  SensorViewController.swift
+//  子猫
+//
+//  Created by 川口剛司 on H29/02/07.
+//  Copyright © 平成29年 川口剛司. All rights reserved.
+//
+
+import UIKit
+
+class SensorViewController: UIViewController {
+    //UIDeviceクラスを呼ぶ
+    let myDevice: UIDevice = UIDevice.current
+    private var myTextField: UITextField!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //近接センサーを有効にする
+        myDevice.isProximityMonitoringEnabled = true
+        
+        //近接センサーの通知設定
+        NotificationCenter.default.addObserver(
+                self,
+                selector: #selector(self.proximitySensorStateDidChange(notification:)),
+                name: NSNotification.Name(rawValue: "UIDeviceProximityStateDidChangeNotification"),
+                object: nil
+        )
+        //近接センサーを通知する
+        NotificationCenter.default.post(
+                name: NSNotification.Name(rawValue: "UIDeviceProximityStateDidChangeNotification"),
+                object: nil
+        )
+    }
+    
+    func proximitySensorStateDidChange(notification: NSNotification){
+        if myDevice.proximityState == true {
+            //近づいた時
+        }else{
+            //離れた時
+        }
+    }
+    
+    
+    
+    
+    // Do any additional setup after loading the view.
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
+    
+}
